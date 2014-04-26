1d24995e85a6f535fc4aa6adacb972758ff6b0fb
Michał Łuszczyk <michal.luszczyk@gmial.com>
2014-04-26 13:54:13 +0200

    set staticfiles

--- heura/urls.py
+++ heura/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = patterns('heura.views',
 	url(r'^$', 'index'),
@@ -8,5 +10,9 @@ urlpatterns = patterns('heura.views',
 	url(r'^get_key/(\w+)$', 'get_key', name='get_key'),
 	url(r'^get_input/(\d+)(?:/(\w*))?$', 'get_input', name='get_input'),
 	url(r'^submit/(\d+)(?:/(\w*))?$', 'submit', name='submit'),
-	url(r'^withdraw/(\w+)$', 'withdraw', name='withdraw')
-)
+	url(r'^withdraw/(\w+)$', 'withdraw', name='withdraw'),
+
+    url(r'^i18n/', include('django.conf.urls.i18n')),
+
+
+) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
